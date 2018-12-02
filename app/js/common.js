d6507ed803f3f892268a23985952f97502492d21
egrechnev <egrechnev@gmail.com>
2018-12-03 01:58:00 +0400

    update

---
+++ app/js/common.js
@@ -0,0 +1,80 @@
+$(function() {
+
+//--Materialize
+	$('.sidenav').sidenav();
+
+	$('.modal').modal();
+
+	$('.materialboxed').materialbox();
+
+//--OwlCarousel
+	$('.about__slides').owlCarousel({
+	    // animateOut: 'slideOutDown',
+	    // animateIn: 'flipInX',
+		items:1,
+	    loop:true,
+	    dots:false,
+	    nav:true
+	});
+
+	$('.services__slides').owlCarousel({
+	    loop:true,
+	    margin:60,
+	    dots:false,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        992:{
+	            items:2
+	        },
+	    }
+	});
+
+	$('.listings__slides').owlCarousel({
+	    loop:true,
+	    // margin:30,
+	    dots:false,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        700:{
+	            items:2
+	        },
+	        1093:{
+	            items:3
+	        },
+	        1321:{
+	            items:4
+	        },
+	    }
+	});
+
+//--Navigation menu links (anchor)
+	$('.menu').on('click','.anchor', function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+
+//--logo up
+  	$(".footer-logo .logo img").click(function() {
+    	$("html, body").animate({
+      		scrollTop: "0"
+    	}, 800)
+  	});
+
+//--Fake links
+  var links = document.querySelectorAll('a[data-fake-link], input[name=submit]');
+  for(var i = 0; i < links.length; i++){
+    links[i].onclick = confirmAway;
+  }
+  function confirmAway(e){
+    return false;
+  }
+
+});
